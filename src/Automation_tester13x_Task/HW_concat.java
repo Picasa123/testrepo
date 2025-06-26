70cab282a57f439fd74e801b7253af46f713f123
AdityaRanjanpandey <adityarun9999@gmail.com>
2025-06-26 08:26:51 +0530

    test program

---
+++ src/Automation_tester13x_Task/HW_concat.java
@@ -0,0 +1,21 @@
+package Automation_tester13x_Task;
+
+public class HW_concat {
+
+
+public static void main(String[] args) {
+
+
+
+
+
+            String s = "Aditya";
+
+                 // Use concat() method for string concatenation
+            s = s.concat("ranjan pandey marriesd");
+            System.out.println(s);
+        }
+
+
+    }
+
